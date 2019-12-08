5637c489010b25b1e5896916e69b24ecbf32f64a
Jiesi Ren <jiesiren@gmail.com>
2019-12-08 11:33:30 -0800

    Add looper for app glue

--- app/src/main/cpp/CatCam.cpp
+++ app/src/main/cpp/CatCam.cpp
@@ -1,11 +1,34 @@
 #include <jni.h>
 #include <string>
-#include "log.h"
+#include <android/looper.h>
+#include <android_native_app_glue.h>
+#include "CatLog.h"
 
+static void appGlueOnAppCmd(struct android_app* app, int32_t cmd) {
+    LOGI("onAppCmd %d", cmd);
+}
 
 extern "C" void android_main(struct android_app* state) {
-    while (1) {
+    state->onAppCmd = appGlueOnAppCmd;
 
+    while (1) {
+        // Read all pending events.
+        int events;
+        struct android_poll_source* source;
+
+        while (ALooper_pollAll(0, nullptr, &events, (void**) &source) >= 0) {
+            // Process this event.
+            if (source != nullptr) {
+                LOGI("Process!");
+                source->process(state, source);
+            }
+
+            // Check if we are exiting.
+            if (state->destroyRequested != 0) {
+                LOGI("Destroy!");
+                return;
+            }
+        }
     }
 }
 
--- app/src/main/java/com/jiesiren/catcam/MainActivity.java
+++ app/src/main/java/com/jiesiren/catcam/MainActivity.java
@@ -3,9 +3,15 @@ package com.jiesiren.catcam;
 import android.app.NativeActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
 import org.jetbrains.annotations.Nullable;
 
 public final class MainActivity extends NativeActivity {
+
+    static {
+        System.loadLibrary("CatCam");
+    }
 
     private final String TAG = "CatCam.MainActivity";
 
@@ -13,10 +19,43 @@ public final class MainActivity extends NativeActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "onCreate()");
-        setContentView(R.layout.activity_main);
+
+        hideSystemUI();
+        View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener
+                (new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        hideSystemUI();
+                    }
+                });
     }
 
-    static {
-        System.loadLibrary("CatCam");
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        Log.i(TAG, "hideSysUI");
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
